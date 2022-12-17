1c627131a732a19440f1095ac2a62ba0fa2f6c7a
Batrakyon <110855171+Batrakyon@users.noreply.github.com>
2022-12-16 22:36:58 -0300

    Update SeachDelete.java

--- SeachDelete.java
+++ SeachDelete.java
@@ -92,7 +92,7 @@ public class SeachDelete {
 		int posicao = buscarCPFDocente(CPF, list_docente);
 
 		if(posicao != -1) {
-			System.out.println("\nO aluno " + list_docente.get(posicao).getNome() + " com o CPF " + CPF + " foi apagado.");
+			System.out.println("\nO docente " + list_docente.get(posicao).getNome() + " com o CPF " + CPF + " foi apagado.");
 			list_docente.remove(posicao);
 
 			if (list_docente.size() < 1) {
@@ -101,6 +101,33 @@ public class SeachDelete {
 
 		}else {
 			System.out.println("\nEsse CPF não existe.");
+		}
+	}
+	
+	public static int buscarMateriaDisci(String materia, ArrayList<Disciplinas> list_disci) {
+
+		for(int i = 0; i < list_disci.size(); i++) {
+			if(list_disci.get(i).getMateria().equals(materia)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+public static void apagarMateriaDisci(String materia, ArrayList<Disciplinas> list_disci, boolean cad_disci) {
+		
+		int posicao = buscarMateriaDisci(materia, list_disci);
+		
+		if(posicao != -1) {
+			System.out.println("\nA materia " + list_disci.get(posicao).getMateria() + " foi apagado.");
+			list_disci.remove(posicao);
+			
+			if(list_disci.size() < 1) {
+				cad_disci = false;
+			}
+			
+		} else {
+			System.out.println("\nEssa materia não existe.");
 		}
 	}
 }
