f60c5424787510cc8d92c03a732323beb88f1a26
Batrakyon <110855171+Batrakyon@users.noreply.github.com>
2022-12-16 22:38:14 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -41,14 +41,15 @@ public class Principal {
 				System.out.println("\nVocê escolheu Gerenciamento de alunos.");
 				System.out.print("Insira o que deseja realizar: ");
 
-				while (gereciamento_aluno != 5){ //menu alunos
+				while (gereciamento_aluno != 6){ //menu alunos
 
 					System.out.println("\n-------------------------------");
 					System.out.println("1 - Adicionar Aluno; ");
 					System.out.println("2 - Registrar Aluno em Turma; ");
-					System.out.println("3 - Modificar informações de Aluno; ");
-					System.out.println("4 - Remover aluno; ");
-					System.out.println("5 - voltar. ");
+					System.out.println("3 - Registrar Aluno em Disciplina; ");
+					System.out.println("4 - Modificar informações de Aluno; ");
+					System.out.println("5 - Remover aluno; ");
+					System.out.println("6 - voltar. ");
 					System.out.println("-------------------------------");
 					System.out.print("\nDigite a opção desejada:");
 					gereciamento_aluno = sc.nextInt();
@@ -77,7 +78,7 @@ public class Principal {
 
 					case 2:
 
-						// colocar a condição de exiostencia da turma aqui
+						// colocar a condição de existencia da turma aqui
 						if(Aluno.getCad_aluno() == true) {
 
 							System.out.println("\nVocê escolheu o registro de aluno em turma");
@@ -98,8 +99,36 @@ public class Principal {
 						} else {
 							System.out.println("\nVocê não possui nenhuma turma ou nenhum aluno cadastratos no sistema.");
 						}
-
+						
+						break;
+						
 					case 3:
+						
+						// colocar a condição de existencia de disciplina aqui
+						if(Aluno.getCad_aluno() == true) {
+
+							System.out.println("\nVocê escolheu o registro de aluno em qual disciplina");
+
+							for (int i = 0; i < list_aluno.size(); i++){
+								System.out.println(list_aluno.get(i).getNome());
+
+							}
+
+							System.out.println("\nDigite o nome do aluno que deseja registrar");
+							String registrar_aluno = sc.next();
+
+							//colocar a disciplina que ele vai se registrar
+
+							System.out.print("Tem certeza? [1 - Sim, 2 - N�o]: ");
+							int certeza = sc.nextInt();
+
+						} else {
+							System.out.println("\nVocê não possui nenhuma disciplina ou nenhum aluno cadastratos no sistema.");
+						}
+						
+						break;
+
+					case 4:
 
 						if(Aluno.getCad_aluno() == true) {
 
@@ -194,7 +223,7 @@ public class Principal {
 
 						break;
 
-					case 4:
+					case 5:
 
 						if(Aluno.getCad_aluno() == true) {
 
@@ -318,7 +347,7 @@ public class Principal {
 						
 					case 2:
 						
-						// colocar a condição de exiostencia da disciplina aqui
+						// colocar a condição de existencia da disciplina aqui
 						if(Docentes.getCad_docente() == true) {
 
 							System.out.println("\nVocê escolheu o registro de docente em disciplina");
@@ -346,7 +375,7 @@ public class Principal {
 
 						if(Docentes.getCad_docente() == true) {
 
-							System.out.println("\nVocê escolheu modificar informação de aluno");
+							System.out.println("\nVocê escolheu modificar informação de docente");
 
 							System.out.println("1 - Nome");
 							System.out.println("2 - CPF");
@@ -419,6 +448,7 @@ public class Principal {
 
 							System.out.println("\nVocê escolheu excluir um docente");
 							System.out.println("Qual CPF deseja excluir?");
+							
 							for(int i = 0; i < list_docente.size(); i++) {
 								System.out.println(list_docente.get(i).getCPF());
 
@@ -433,7 +463,7 @@ public class Principal {
 							if(certeza == 1) {
 								SeachDelete.apagarCPFAluno(escolher_CPF, list_aluno, Aluno.getCad_aluno());
 
-								if(list_aluno.size() < 1) {
+								if(list_docente.size() < 1) {
 									docente.setCad_docente(false);
 								}
 
@@ -445,6 +475,7 @@ public class Principal {
 						} else {
 							System.out.println("\nNão existem nenhum docente registrado");
 						}
+						
 						break;
 						
 						default:
@@ -475,9 +506,165 @@ public class Principal {
 					System.out.print("\nDigite a opção desejada:");
 					gereciamento_disci = sc.nextInt();
 					
-				}
+					switch(gereciamento_disci){
 
+					case 1:
+
+						System.out.println("\nVocê escolheu cadastro de disciplina");
+						System.out.print("\nInsira o nome da disciplina: ");
+						String materia = sc.next();
+						
+						disci = new Disciplinas(materia);
+						disci.setCad_disci(true);
+						list_disci.add(disci);
+
+						System.out.println("Disciplina " + disci.getMateria() + " cadastrada!");
+
+						break;
+						
+					case 2:
+						
+					    // colocar a condição de existencia de turma
+						if(Disciplinas.getCad_disci() == true) {
+
+							System.out.println("\nVocê escolheu o registro de docente em disciplina");
+
+							for (int i = 0; i < list_disci.size(); i++){
+								System.out.println(list_disci.get(i).getMateria());
+
+							}
+							
+							System.out.println("\nDigite o nome da diciplina que deseja registrar");
+							String registrar_disci = sc.next();
+
+							//colocar a turma que ele vai se registrar
+
+							System.out.print("Tem certeza? [1 - Sim, 2 - N�o]: ");
+							int certeza = sc.nextInt();
+						} else {
+							System.out.println("\nVocê não possui nenhuma disciplina ou nenhuma turma cadastratas no sistema.");
+						}
+						
+						break;
+						
+					case 3:
+						
+						if(Disciplinas.getCad_disci() == true) {
+
+							System.out.println("\nVocê escolheu modificar informação de disciplinas");
+							System.out.println("Digite qual materia deseja modificar");
+							
+							for (int i = 0; i < list_disci.size(); i++){
+								System.out.println(list_disci.get(i).getMateria());
+
+							}
+							
+							System.out.println("\nDigite sua opção: ");
+							String escolher_materia = sc.next();
+							
+							if(SeachDelete.buscarMateriaDisci(escolher_materia, list_disci) > -1) {
+
+								System.out.println("Digite a nova materia: ");
+								disci.setMateria(sc.next());
+
+								System.out.println("\nA nova materia é: " + disci.getMateria());
+							} else {
+								System.out.println("Essa materia não existe.");
+							}
+							
+						} else {
+							System.out.println("\nNão existem nenhuma disciplina registrada");
+						}
+						
+						break;
+						
+					case 4:
+						
+						if(Disciplinas.getCad_disci() == true) {
+
+							System.out.println("\nVocê escolheu excluir uma disciplina");
+							System.out.println("Qual materia deseja excluir?");
+							
+							for(int i = 0; i < list_disci.size(); i++) {
+								System.out.println(list_disci.get(i).getMateria());
+
+							}
+							
+							System.out.println("\nDigite sua escolha: ");
+							String escolher_materia = sc.next();
+							
+							System.out.println("Tem certeza que é esse que deseja excluir? [1 - sim, 2 - não]");
+							int certeza = sc.nextInt();
+							
+							if(certeza == 1) {
+								SeachDelete.apagarMateriaDisci(escolher_materia, list_disci, Disciplinas.getCad_disci());
+
+								if(list_disci.size() < 1) {
+									disci.setCad_disci(false);
+								}
+
+							}else {
+								System.out.println("\nExclução cancelada");
+								
+							}
+							
+						}else {
+							System.out.println("\nNão existem nenhuma diciplina registrada");
+						}
+						
+						break;
+						
+						default:
+							
+					}
+					
+				}
+				
+				break;
+				
 			case 4:
+				
+				int gereciamento_turma= 0;
+				Turma turma = new Turma(null, 0, 0);
+
+				System.out.println("\nVocê escolheu Gerenciamento de turmas.");
+				System.out.print("Insira o que deseja realizar: ");
+				
+				while (gereciamento_turma != 5){ //menu docentes
+
+					System.out.println("\n-------------------------------");
+					System.out.println("1 - Adicionar Turma; ");
+					System.out.println("2 - Registrar Docente em Disciplina; ");
+					System.out.println("3 - Modificar informações de Docente; ");
+					System.out.println("4 - Remover Doscente; ");
+					System.out.println("5 - voltar. ");
+					System.out.println("-------------------------------");
+					System.out.print("\nDigite a opção desejada:");
+					gereciamento_turma = sc.nextInt();
+					
+					switch(gereciamento_turma){
+
+					case 1:
+
+						System.out.println("\nVocê escolheu cadastro de turma");
+						System.out.print("\nInsira a serie dessa turma: ");
+						String serie = sc.next();
+						
+						System.out.println("Insira o ano de registro dess turma: ");
+						int ano = sc.nextInt();
+						
+						System.out.println("Insira o codigo dessa turma: ");
+						int codigo = sc.nextInt();
+						
+						turma = new Turma(serie, ano, codigo);
+						turma.setCad_turma(true);
+						list_turma.add(turma);
+
+						System.out.println("A turma" + turma.getSerie() + " doe  cadastrada!");
+						
+						break;
+					}
+				}
 
 			case 5:
 				System.out.println("Você escolheu visualizar os relatórios.");
