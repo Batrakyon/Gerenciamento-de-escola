8af5030900f8f693f37653223011dc9df2118748
Batrakyon <110855171+Batrakyon@users.noreply.github.com>
2022-12-16 22:35:57 -0300

    Update Disciplinas.java

--- Disciplinas.java
+++ Disciplinas.java
@@ -1,14 +1,23 @@
 
 public class Disciplinas {
 	
-	public String materia;
+	private String materia;
+	private static boolean cad_disci = false;
 
 	public String getMateria() {
 		return materia;
 	}
 
-	public void setNome(String materia) {
+	public void setMateria(String materia) {
 		this.materia = materia;
+	}
+	
+	public static boolean getCad_disci() {
+		return cad_disci;
+	}
+
+	public static void setCad_disci(boolean cad_disci) {
+		Disciplinas.cad_disci = cad_disci;
 	}
 	
 	public Disciplinas(String materia) {
